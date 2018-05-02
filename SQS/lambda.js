cf3e358c1308a45af29666e081b6554d0dcb6489
Kumudikatest <37790188+Kumudikatest@users.noreply.github.com>
2018-05-02 13:10:33 +0530

    sqs

--- SQS/lambda.js
+++ SQS/lambda.js
@@ -8,7 +8,7 @@ exports.handler = function (event, context, callback) {
 		MaxNumberOfMessages: '1',
 		VisibilityTimeout: '30',
 		WaitTimeSeconds: '0'
-	}, function (receivedMessages) {
+	}, function (receiveMessages) {
 		receivedMessages.forEach(message => {
 			// your logic to access each message through out the loop. Each message is available under variable message 
 			// within this block
